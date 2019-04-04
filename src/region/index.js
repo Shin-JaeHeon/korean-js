17ee858947e1d51901168a769d42e9c9c2849f04
Shin-JaeHeon <shinjaeheon520614@gmail.com>
2019-04-04 20:33:22 +0900

    Initial commit

---
+++ src/region/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const region_kr_1 = require("region-name-kr/region-kr");
+class Region extends region_kr_1.default {
+}
+exports.default = Region;
---
+++ src/region/index.ts
@@ -0,0 +1,5 @@
+import RegionKr from 'region-name-kr/region-kr';
+
+export default class Region extends RegionKr {
+
+}
