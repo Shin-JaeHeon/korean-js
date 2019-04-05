2d6145154d34eb3ca6c344b9e7bd737429b40df3
Shin-JaeHeon <shinjaeheon520614@gmail.com>
2019-04-05 11:21:49 +0900

    add enum NumberMode

---
+++ src/number/mode.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumberMode;
+(function (NumberMode) {
+    NumberMode[NumberMode["Unit4"] = 2019] = "Unit4";
+    NumberMode[NumberMode["KoreanUnit"] = 2019] = "KoreanUnit";
+    NumberMode[NumberMode["Unit"] = 2021] = "Unit";
+})(NumberMode || (NumberMode = {}));
+exports.default = NumberMode;
---
+++ src/number/mode.ts
@@ -0,0 +1,8 @@
+enum NumberMode {
+  Unit4 = 2019,
+  KoreanUnit = 2019,
+  Unit = 2021
+
+}
+
+export default NumberMode;
