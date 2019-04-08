eaf748abee643c858291b1f1cd48c27b65dded30
Shin-JaeHeon <shinjaeheon520614@gmail.com>
2019-04-08 09:28:48 +0900

    add toNumber in src/number/index.js, release v0.3.0

--- src/number/index.ts
+++ src/number/index.ts
@@ -2,7 +2,7 @@ import NumberMode from './mode';
 
 export default class Number {
   private static koreanName = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
-  private static koreanUnitName = ['십', '백', '천', '', '만', '억', '조', '경'];
+  private static koreanUnitName = ['십', '백', '천', '', '만', '억', '조'];
 
   static toKorean(n: number, mode?: NumberMode): string {
     let temp = n.toString().split('');
@@ -25,6 +25,49 @@ export default class Number {
         }
       }
     }).reverse();
-    return temp.join('').replace(/영.?/g, '').replace(/0.?/g, '').replace(/일(.)/g, '$1');
+    return temp.join('').replace(/[영0].?/g, '').replace(/일([십백천])/g, '$1');
+  }
+
+  static toNumber(str: string) {
+    const temp = str.replace(/천백/g, '천일백').replace(/([백])(십)/g, '백일십').split('');
+    let sum = 0;
+    let is4 = false;
+    let digit = 0;
+    return temp.reduce((prev, current, index) => {
+      let tempSum = parseInt(prev.join(''));
+      switch (this.koreanUnitName.indexOf(current)) {
+        case 2:
+          prev[1] = 0;
+        case 1:
+          prev[2] = 0;
+        case 0:
+          prev[3] = 0;
+          break;
+        case 3:
+          break;
+        case 4:
+          tempSum *= 10000;
+          is4 = true;
+          break;
+        case 5:
+          tempSum *= (10 ** 8);
+          is4 = true;
+          break;
+        case 6:
+          tempSum *= (10 ** 12);
+          is4 = true;
+          break;
+        case -1:
+          prev[digit++] = (this.koreanName.indexOf(current));
+          break;
+      }
+      if (is4) {
+        sum += tempSum;
+        prev = [0];
+        is4 = false;
+        digit = 0;
+      }
+      return index === temp.length - 1 ? sum + parseInt(prev.join('')) : prev;
+    }, [0]);
   }
 }
